3301a62c5f7f1ddb801c9c53c58f70f8e31f260c
Piero Lescano <me@pierolescano.com>
2025-01-04 10:31:11 -0500

    feat: Agregar validacion de sesiones

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,60 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import {
+  readSessionCookie,
+  validateSessionToken,
+  deleteSessionTokenCookie,
+  setSessionTokenCookie,
+} from "./lib/session";
 
 dotenv.config();
 
+const ALLOWED_ORIGIN = "http://localhost:3000";
+
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+// CSRF protection
+app.use((req, res, next) => {
+  if (req.method === "GET") {
+    next();
+    return;
+  }
+
+  const origin = req.headers.origin ?? null;
+  if (origin === null || origin != ALLOWED_ORIGIN) {
+    res.status(403).end();
+    return;
+  }
+
+  next();
+  return;
+});
+
+// Session validation
+app.use(async (req, res, next) => {
+  const token = readSessionCookie(req.headers.cookie ?? "");
+  if (!token) {
+    res.locals.user = null;
+    res.locals.session = null;
+    return next();
+  }
+
+  const { session, user } = await validateSessionToken(token);
+  if (session === null) {
+    deleteSessionTokenCookie(res);
+    res.locals.user = null;
+    res.locals.session = null;
+    return next();
+  }
+
+  setSessionTokenCookie(res, token, session.expiresAt);
+
+  res.locals.session = session;
+  res.locals.user = user;
+
+  return next();
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Grow Analytics");
---
+++ backend/src/lib/session.ts
@@ -0,0 +1,122 @@
+import type { User, Session } from "@prisma/client";
+import type { Response } from "express";
+import {
+  encodeBase32LowerCaseNoPadding,
+  encodeHexLowerCase,
+} from "@oslojs/encoding";
+import { prisma } from "./db.js";
+import { sha256 } from "@oslojs/crypto/sha2";
+
+const PROD = "production";
+
+export function generateSessionToken(): string {
+  const bytes = new Uint8Array(20);
+  crypto.getRandomValues(bytes);
+  const token = encodeBase32LowerCaseNoPadding(bytes);
+  return token;
+}
+
+export async function createSession(
+  token: string,
+  userId: number,
+): Promise<Session> {
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const session: Session = {
+    id: sessionId,
+    userId,
+    expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+  };
+  await prisma.session.create({
+    data: session,
+  });
+  return session;
+}
+
+export async function validateSessionToken(
+  token: string,
+): Promise<SessionValidationResult> {
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const result = await prisma.session.findUnique({
+    where: {
+      id: sessionId,
+    },
+    include: {
+      user: true,
+    },
+  });
+  if (result === null) {
+    return { session: null, user: null };
+  }
+  const { user, ...session } = result;
+  if (Date.now() >= session.expiresAt.getTime()) {
+    await prisma.session.delete({ where: { id: sessionId } });
+    return { session: null, user: null };
+  }
+  if (Date.now() >= session.expiresAt.getTime() - 1000 * 60 * 60 * 24 * 15) {
+    session.expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
+    await prisma.session.update({
+      where: {
+        id: session.id,
+      },
+      data: {
+        expiresAt: session.expiresAt,
+      },
+    });
+  }
+  return { session, user };
+}
+
+export async function invalidateSession(sessionId: string): Promise<void> {
+  await prisma.session.delete({ where: { id: sessionId } });
+}
+
+export function setSessionTokenCookie(
+  res: Response,
+  token: string,
+  expiresAt: Date,
+): void {
+  if (process.env.NODE_ENV === PROD) {
+    res.appendHeader(
+      "Set-Cookie",
+      `session=${token}; HttpOnly; SameSite=Lax; Expires=${expiresAt.toUTCString()}; Path=/; Secure;`,
+    );
+  } else {
+    res.appendHeader(
+      "Set-Cookie",
+      `session=${token}; HttpOnly; SameSite=Lax; Expires=${expiresAt.toUTCString()}; Path=/`,
+    );
+  }
+}
+
+export function deleteSessionTokenCookie(res: Response): void {
+  if (process.env.NODE_ENV === PROD) {
+    res.appendHeader(
+      "Set-Cookie",
+      "session=; HttpOnly; SameSite=Lax; Max-Age=0; Path=/; Secure;",
+    );
+  } else {
+    res.appendHeader(
+      "Set-Cookie",
+      "session=; HttpOnly; SameSite=Lax; Max-Age=0; Path=/",
+    );
+  }
+}
+
+export function readSessionCookie(cookieHeader: string): string | null {
+  const cookies = new Map<string, string>();
+  const items = cookieHeader.split("; ");
+
+  for (const item of items) {
+    const pair = item.split("=");
+    const rawKey = pair[0];
+    const rawValue = pair[1] ?? "";
+    if (!rawKey) continue;
+    cookies.set(decodeURIComponent(rawKey), decodeURIComponent(rawValue));
+  }
+
+  return cookies.get("session") ?? null;
+}
+
+export type SessionValidationResult =
+  | { session: Session; user: User }
+  | { session: null; user: null };
