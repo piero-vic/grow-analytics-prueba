2dd8f1a96c4194e1654c57507cac5106606c5d7d
Piero Lescano <me@pierolescano.com>
2025-01-03 22:21:53 -0500

    feat: Agregar login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  FacebookFilled,
+  GoogleOutlined,
+  TwitterOutlined,
+} from "@ant-design/icons";
+import { Layout, Button, Form, Input, Flex, Checkbox, Typography } from "antd";
+const { Title } = Typography;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Layout style={{ minHeight: "100vh" }}>
+      <Layout.Content
+        style={{ margin: "24px 16px", display: "flex", alignItems: "center" }}
+      >
+        <Flex
+          vertical
+          align="center"
+          justify="center"
+          style={{ width: "100%", maxWidth: 600, marginInline: "auto" }}
+        >
+          <Title>Sign up</Title>
+
+          <Form name="login" style={{ width: "100%" }}>
+            <Form.Item
+              name="email"
+              rules={[{ required: true, message: "Please input your email!" }]}
+            >
+              <Input type="email" placeholder="Email" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input type="password" placeholder="Password" />
+            </Form.Item>
+
+            <Form.Item>
+              <Form.Item name="remember" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+            </Form.Item>
+
+            <Form.Item>
+              <Button block type="primary" size="large" htmlType="submit">
+                Log in
+              </Button>
+            </Form.Item>
+          </Form>
+
+          <Flex vertical gap="small" align="center">
+            Or login with
+            <Flex gap="small">
+              <Button
+                size="large"
+                icon={<FacebookFilled style={{ color: "#1877F2" }} />}
+              />
+              <Button
+                size="large"
+                icon={<TwitterOutlined style={{ color: "#3399FF" }} />}
+              />
+              <Button
+                size="large"
+                icon={<GoogleOutlined style={{ color: "#EA4335" }} />}
+              />
+            </Flex>
+          </Flex>
+        </Flex>
+      </Layout.Content>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
