e6b6979b32142c82389c840bec2ab785b2f6ff23
Piero Lescano <me@pierolescano.com>
2025-01-05 19:10:41 -0500

    feat: Agregar funcionalidad para filtrar usuarios

--- frontend/src/routes/Users.tsx
+++ frontend/src/routes/Users.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { DeleteFilled, EditFilled } from "@ant-design/icons";
+import { DeleteFilled, EditFilled, SearchOutlined } from "@ant-design/icons";
 import {
   Layout,
   Flex,
@@ -8,193 +8,112 @@ import {
   Table,
   Space,
   Button,
-  type GetProp,
-  type TableProps,
   Modal,
   Input,
+  type TableColumnType,
+  type TablePaginationConfig,
 } from "antd";
-import type { SorterResult } from "antd/es/table/interface";
 
 const { Title } = Typography;
 const { Column } = Table;
 
-type TablePaginationConfig = Exclude<
-  GetProp<TableProps, "pagination">,
-  boolean
->;
-
-interface DataType {
+type User = {
   id: number;
   username: string;
   email: string;
   name: string;
   paternalLastName: string;
   maternalLastName: string;
-}
+};
 
-interface TableParams {
-  pagination?: TablePaginationConfig;
-  sortField?: SorterResult<any>["field"];
-  sortOrder?: SorterResult<any>["order"];
-  filters?: Parameters<GetProp<TableProps, "onChange">>[1];
-}
+type DataIndex = keyof User;
 
 const Users: React.FC = () => {
-  const [data, setData] = useState<DataType[]>();
+  const [data, setData] = useState<User[]>();
   const [loading, setLoading] = useState(false);
-  const [tableParams, setTableParams] = useState<TableParams>({
-    pagination: {
-      current: 1,
-      pageSize: 10,
-    },
+  const [pagination, setPagination] = useState<TablePaginationConfig>({
+    current: 1,
+    pageSize: 10,
   });
+
+  const { current: currentPage } = pagination;
 
   const fetchData = () => {
     setLoading(true);
-    fetch(
-      `http://localhost:3000/users?page=${tableParams.pagination?.current || "1"}&size=10`,
-    )
+    fetch(`http://localhost:3000/users?page=${currentPage || "1"}&size=10`)
       .then((res) => res.json())
       .then(({ results, totalCount }) => {
         setData(results);
         setLoading(false);
-        setTableParams({
-          ...tableParams,
-          pagination: {
-            ...tableParams.pagination,
-            total: totalCount,
-          },
-        });
+        setPagination((state) => ({ ...state, total: totalCount }));
       });
   };
 
-  useEffect(fetchData, [
-    tableParams.pagination?.current,
-    tableParams.pagination?.pageSize,
-    tableParams?.sortOrder,
-    tableParams?.sortField,
-    JSON.stringify(tableParams.filters),
-  ]);
+  useEffect(fetchData, [currentPage]);
 
-  const handleTableChange: TableProps<DataType>["onChange"] = (
-    pagination,
-    filters,
-    sorter,
-  ) => {
-    setTableParams({
-      pagination,
-      filters,
-      sortOrder: Array.isArray(sorter) ? undefined : sorter.order,
-      sortField: Array.isArray(sorter) ? undefined : sorter.field,
-    });
-
-    if (pagination.pageSize !== tableParams.pagination?.pageSize) {
-      setData([]);
-    }
-  };
-
-  // NOTE: Eliminar usuarios
-  const deleteUser = (id: number) => {
-    Modal.confirm({
-      title: "¿Quieres eliminar este usuario?",
-      okText: "Eliminar",
-      okButtonProps: { variant: "solid", color: "danger" },
-      cancelText: "Cancelar",
-      onOk: async () => {
-        const res = await fetch(`http://localhost:3000/users/${id}`, {
-          method: "DELETE",
-        });
-
-        if (res.ok) {
-          fetchData();
-        }
-      },
-    });
-  };
-
-  // NOTE: Editar usuarios
-  const openModal = (userData: DataType) => {
-    const modal = Modal.info({});
-
-    modal.update({
-      icon: null,
-      title: "Editar usuario",
-      okText: "Guardar",
-      cancelText: "Cancelar",
-      okButtonProps: { htmlType: "submit" },
-      modalRender: (dom) => (
-        <Form
-          name="edit-user"
-          initialValues={userData}
-          style={{ width: "100%" }}
-          onFinish={async (values) => {
-            const res = await fetch(
-              `http://localhost:3000/users/${userData.id}`,
-              {
-                method: "PUT",
-                body: JSON.stringify(values),
-                headers: new Headers({ "content-type": "application/json" }),
-              },
-            );
-
-            if (res.ok) {
-              fetchData();
-            }
+  // NOTE: Función para obtener las props necesarias para la funcionalidad de filtrado
+  const getColumnSearchProps = (
+    dataIndex: DataIndex,
+  ): TableColumnType<User> => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          placeholder="Buscar"
+          value={selectedKeys[0]}
+          onChange={(e) => {
+            setSelectedKeys(e.target.value ? [e.target.value] : []);
           }}
-        >
-          {dom}
-        </Form>
-      ),
-      content: (
-        <>
-          <Form.Item
-            name="username"
-            rules={[{ required: true, message: "Please input your username!" }]}
+          onPressEnter={() => confirm()}
+          style={{ marginBottom: 8, display: "block" }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => confirm()}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
           >
-            <Input placeholder="Username" />
-          </Form.Item>
-
-          <Form.Item
-            name="name"
-            rules={[{ required: true, message: "Please input your name!" }]}
+            Search
+          </Button>
+          <Button
+            onClick={() => {
+              clearFilters?.();
+              confirm();
+              close();
+            }}
+            size="small"
+            style={{ width: 90 }}
           >
-            <Input placeholder="Name" />
-          </Form.Item>
-
-          <Form.Item
-            name="paternalLastName"
-            rules={[
-              {
-                required: true,
-                message: "Please input your paternal last name!",
-              },
-            ]}
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
           >
-            <Input placeholder="Paternal last name" />
-          </Form.Item>
-
-          <Form.Item
-            name="maternalLastName"
-            rules={[
-              {
-                required: true,
-                message: "Please input your maternal last name!",
-              },
-            ]}
-          >
-            <Input placeholder="Maternal last name" />
-          </Form.Item>
-
-          <Form.Item
-            name="email"
-            rules={[{ required: true, message: "Please input your email!" }]}
-          >
-            <Input type="email" placeholder="Email" />
-          </Form.Item>
-        </>
-      ),
-    });
-  };
+            Close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+  });
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
@@ -208,36 +127,52 @@ const Users: React.FC = () => {
           style={{ width: "100%", marginInline: "auto" }}
         >
           <Title style={{ textAlign: "center" }}>Usuarios</Title>
-          <Table<DataType>
+          <Table<User>
             rowKey={(record) => record.id}
             dataSource={data}
-            pagination={tableParams.pagination}
+            pagination={pagination}
             loading={loading}
-            onChange={handleTableChange}
+            onChange={(pagination) => {
+              setPagination(pagination);
+            }}
             style={{ width: "100%" }}
+            scroll={{ x: "max-content" }}
           >
-            <Column<DataType>
+            <Column<User>
               title="ID"
               dataIndex="id"
               key="id"
               sorter={(a, b) => a.id - b.id}
+              {...getColumnSearchProps("id")}
             />
-            <Column<DataType>
-              title="Email"
-              dataIndex="email"
-              key="email"
-              sorter={(a, b) => (a.email > b.email ? 1 : 0)}
-            />
-            <Column<DataType>
+
+            <Column<User>
               title="Name"
               dataIndex="name"
               key="name"
-              sorter={(a, b) => (a.name > b.name ? 1 : 0)}
+              sorter={(a, b) => a.name.localeCompare(b.name)}
               render={(_, record) =>
                 `${record.name} ${record.paternalLastName} ${record.maternalLastName}`
               }
+              {...getColumnSearchProps("name")}
+              // NOTE: Filtro personalizado para poder filtrar el nombre completo
+              onFilter={(value, record) =>
+                `${record.name} ${record.paternalLastName} ${record.maternalLastName}`
+                  .toString()
+                  .toLowerCase()
+                  .includes((value as string).toLowerCase())
+              }
             />
-            <Column<DataType>
+
+            <Column<User>
+              title="Email"
+              dataIndex="email"
+              key="email"
+              sorter={(a, b) => a.email.localeCompare(b.email)}
+              {...getColumnSearchProps("email")}
+            />
+
+            <Column<User>
               title="Action"
               dataIndex="action"
               key="action"
@@ -247,13 +182,13 @@ const Users: React.FC = () => {
                     variant="outlined"
                     color="primary"
                     icon={<EditFilled />}
-                    onClick={() => openModal(record)}
+                    onClick={() => openUserEditModal(record, fetchData)}
                   />
                   <Button
                     variant="outlined"
                     color="danger"
                     icon={<DeleteFilled />}
-                    onClick={() => deleteUser(record.id)}
+                    onClick={() => openUserDeleteModal(record.id, fetchData)}
                   />
                 </Space>
               )}
@@ -263,6 +198,102 @@ const Users: React.FC = () => {
       </Layout.Content>
     </Layout>
   );
+};
+
+const openUserDeleteModal = (id: number, onSucess: () => void) => {
+  Modal.confirm({
+    title: "¿Quieres eliminar este usuario?",
+    okText: "Eliminar",
+    okButtonProps: { variant: "solid", color: "danger" },
+    cancelText: "Cancelar",
+    onOk: async () => {
+      const res = await fetch(`http://localhost:3000/users/${id}`, {
+        method: "DELETE",
+      });
+
+      if (res.ok) {
+        onSucess();
+      }
+    },
+  });
+};
+
+const openUserEditModal = (user: User, onSucess: () => void) => {
+  Modal.info({
+    icon: null,
+    title: "Editar usuario",
+    okText: "Guardar",
+    cancelText: "Cancelar",
+    okButtonProps: { htmlType: "submit" },
+    modalRender: (dom) => (
+      <Form
+        name="edit-user"
+        initialValues={user}
+        style={{ width: "100%" }}
+        onFinish={async (values) => {
+          const res = await fetch(`http://localhost:3000/users/${user.id}`, {
+            method: "PUT",
+            body: JSON.stringify(values),
+            headers: new Headers({ "content-type": "application/json" }),
+          });
+
+          if (res.ok) {
+            onSucess();
+          }
+        }}
+      >
+        {dom}
+      </Form>
+    ),
+    content: (
+      <>
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input placeholder="Username" />
+        </Form.Item>
+
+        <Form.Item
+          name="name"
+          rules={[{ required: true, message: "Please input your name!" }]}
+        >
+          <Input placeholder="Name" />
+        </Form.Item>
+
+        <Form.Item
+          name="paternalLastName"
+          rules={[
+            {
+              required: true,
+              message: "Please input your paternal last name!",
+            },
+          ]}
+        >
+          <Input placeholder="Paternal last name" />
+        </Form.Item>
+
+        <Form.Item
+          name="maternalLastName"
+          rules={[
+            {
+              required: true,
+              message: "Please input your maternal last name!",
+            },
+          ]}
+        >
+          <Input placeholder="Maternal last name" />
+        </Form.Item>
+
+        <Form.Item
+          name="email"
+          rules={[{ required: true, message: "Please input your email!" }]}
+        >
+          <Input type="email" placeholder="Email" />
+        </Form.Item>
+      </>
+    ),
+  });
 };
 
 export default Users;
